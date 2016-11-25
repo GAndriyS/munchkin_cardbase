f2b52d9fd7e862ccd459d80799df4f0754c12986
Andrii Gorbatiuk <andrii_gorbatiuk@epam.com>
2016-11-25 10:37:37 +0200

    seperate cards to door and treasure on server and MongoDB

--- routes/cards.js
+++ routes/cards.js
@@ -3,11 +3,21 @@
 let express = require('express');
 let mongojs = require('mongojs');
 let router = express.Router();
-let db = mongojs('mongodb://admin:admin@ds153667.mlab.com:53667/munchkin_cardbase', ['cards']);
+let db = mongojs('mongodb://admin:admin@ds153667.mlab.com:53667/munchkin_cardbase', ['door_cards', 'treasure_cards']);
 
-// Get All Cards
-router.get('/cards', (req, res, next) => {
-  db.cards.find((err, cards) => {
+// Get All Door Cards
+router.get('/door/cards', (req, res, next) => {
+  db.door_cards.find((err, cards) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(cards);
+  });
+});
+
+// Get All Treasure Cards
+router.get('/treasure/cards', (req, res, next) => {
+  db.treasure_cards.find((err, cards) => {
     if (err) {
       res.send(err);
     }
